703119845ff107e6221ed5aa2d4ac99b09a0ecdc
Nymika <p.nymika@gmail.com>
2021-03-12 15:27:44 +0530

    search, dropdown are done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    contests: [],
+    search: '',
+    filter: 'all'
+  }
+
+  searchSpace = (event) => {
+    this.setState({ 
+      search: event.target.value 
+    })
+  }
+
+  filterData = (event) => {
+    this.setState({
+      filter: event.target.value
+    })
+  }
+
+  GetListOfContests() {
+    axios.get('https://codeforces.com/api/contest.list')
+      .then(response => {
+        this.setState({
+          contests: response.data.result,
+        })
+      }).catch((e) => alert(e))
+  }
+
+  componentDidMount() {
+    this.GetListOfContests()
+  }
+
+  render() {
+
+    let ICPCList = this.state.contests.filter(i => {
+      return i.type === "ICPC";
+    }).map(i => {
+      return (
+        <p key={i.id}>{i.name} - {i.type}</p>
+      )
+    })
+
+    let CFList = this.state.contests.filter(i => {
+      return i.type === "CF";
+    }).map(i => {
+      return (
+        <p key={i.id}>{i.name} - {i.type}</p>
+      )
+    })
+
+    let SearchList = this.state.contests.filter(data => {
+      return data.name.toLowerCase().includes(this.state.search.toLowerCase())
+    }).map(i => {
+      return (
+        <p key={i.id}>{i.name} - {i.type}</p>
+      )
+    })
+
+    const AllContestsList = this.state.contests.map(i => {
+      return (
+        <div>
+          <p key={i.id}>{i.name} - {i.type}</p>
+        </div>
+      )
+    })
+
+    const displayContests = () => {
+      if (this.state.search !== '')
+        return SearchList
+      if (this.state.filter === 'icpc')
+        return ICPCList
+      else if (this.state.filter === 'cf')
+        return CFList
+      else
+        return AllContestsList
+    }
+
+    return (
+      <div className="App">
+        <header className="App-header">
+
+          <select value={this.state.filter} onChange={(e) => this.filterData(e)}>
+            <option value="icpc">ICPC</option>
+            <option value="cf">CF</option>
+            <option value="all">All</option>
+          </select>
+
+          <input type="text" placeholder="Enter item to be searched" onChange={(e) => this.searchSpace(e)} />
+
+          {displayContests()}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
